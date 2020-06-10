326137c4553684a1b8555896ba680ac9ca59a4d4
maxwellrk <maxkatz23@gmail.com>
2020-06-10 19:42:42 -0400

    Added dice shuffle logic, made held a propery on dice

--- src/components/GameInstance.jsx
+++ src/components/GameInstance.jsx
@@ -2,22 +2,42 @@ import React, { useState } from 'react';
 import Die from './Die';
 
 const GameInstance = (props) => {
-  let [currentDice, changeCurrentDice] = useState([
-    { value: 1, color: 'black' },
-    { value: 1, color: 'black' },
-    { value: 1, color: 'black' },
-    { value: 1, color: 'black' },
-    { value: 1, color: 'black' },
-    { value: 1, color: 'black' },
+  const [currentDice, changeCurrentDice] = useState([
+    { value: 1, color: 'black', held: false },
+    { value: 1, color: 'black', held: false },
+    { value: 1, color: 'black', held: false },
+    { value: 1, color: 'black', held: false },
+    { value: 1, color: 'black', held: false },
+    { value: 1, color: 'black', held: false },
   ]);
 
-  let [heldDice, changeHeldDice] = useState([]);
+  const potentialDice = [
+    { value: 1, color: 'black', held: false },
+    { value: 2, color: 'red', held: false },
+    { value: 3, color: 'green', held: false },
+    { value: 4, color: 'green', held: false },
+    { value: 5, color: 'red', held: false },
+    { value: 6, color: 'black', held: false },
+  ];
+
+  const selectRandomDie = () => {
+    return potentialDice[Math.floor(Math.random() * potentialDice.length)];
+  };
+
+  const diceShuffle = () => {
+    changeCurrentDice(
+      currentDice.map((die) => {
+        return die.held ? die : selectRandomDie();
+      })
+    );
+  };
 
   return (
     <div>
       {currentDice.map((die, index) => {
         return <Die value={die.value} color={die.color} key={index} />;
       })}
+      <button onClick={() => diceShuffle()}>SHUFFLE DICE</button>
     </div>
   );
 };
