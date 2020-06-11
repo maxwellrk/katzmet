e288efe4146b40dc2f2673f0f5438e91ff0dddf3
maxwellrk <maxkatz23@gmail.com>
2020-06-10 22:36:31 -0400

    added button to handle dieHolding

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Die = ({ value, color }) => {
-  return <div>{value + ' ' + color}</div>;
+const Die = ({ value, color, held, holdDie, position }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          holdDie(position);
+        }}
+      >
+        {held ? 'RETURN' : 'HOLD'}
+      </button>
+      <p style={{ color, display: 'inline' }}>DIE: {value}</p>
+    </div>
+  );
 };
 
 export default Die;
